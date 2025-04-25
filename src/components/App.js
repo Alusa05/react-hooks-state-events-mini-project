8c65a49203a2ef246a5dbf6679d8af11a2e9c21c
Lisa Alusa <sayilisaalusa@gmail.com>
2025-04-25 09:11:13 +0300

    modified files

--- src/components/App.js
+++ src/components/App.js
@@ -8,12 +8,34 @@ console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [tasks, setTasks] = React.useState(TASKS);
+  const [categories] = React.useState(CATEGORIES);
+  const [selectedCategory, setSelectedCategory] = React.useState("All");
+
+  function handleDeleteTask(taskText){
+    setTasks(tasks.filter((task) => task.text !== taskText));
+  }
+  function handleAddTask(newTask){
+    setTasks([...tasks, newTask]);
+  }
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter 
+      categories={categories} 
+      selectedCategory={selectedCategory} 
+      onSelectCategory={setSelectedCategory} 
+      />
+      
+      <TaskList 
+        tasks={tasks} 
+        onDeleteTask={handleDeleteTask}
+        selectedCategory={selectedCategory}
+      />
+      <NewTaskForm
+      categories={categories.filter(cat => cat !== "All")}
+      onTaskFormSubmit={handleAddTask}
+       />
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,23 @@
 import React from "react";
+import Task from "./Task"; // Make sure you have a Task component
 
-function TaskList() {
+function TaskList({ tasks, onDeleteTask, selectedCategory }) {
+  // Filter tasks based on selected category
+  const displayedTasks = selectedCategory === "All" 
+    ? tasks 
+    : tasks.filter(task => task.category === selectedCategory);
+
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {displayedTasks.map(task => (
+        <Task 
+          key={task.text} 
+          task={task} 
+          onDelete={onDeleteTask}
+        />
+      ))}
     </div>
   );
 }
 
-export default TaskList;
+export default TaskList; // This was missing before
